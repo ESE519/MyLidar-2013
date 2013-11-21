906ff3669a407e8010e18421089dd9de7b7d6380
thisischerry <jeff@ubuntu.(none)>
2013-11-20 23:04:27 -0500

    code that can control mail car

---
+++ mailcar_0/CMakeFiles/control_car_keyboard.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/control_car_keyboard.dir/src/control_car_keyboard.cpp.o"
+  "/home/jeff/catkin_ws/devel/lib/mailcar_0/control_car_keyboard.pdb"
+  "/home/jeff/catkin_ws/devel/lib/mailcar_0/control_car_keyboard"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/control_car_keyboard.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ mailcar_0/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jeff/catkin_ws/src/mailcar_0
+# Build directory: /home/jeff/catkin_ws/build/mailcar_0
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
