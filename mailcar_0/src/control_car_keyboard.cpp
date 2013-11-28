a2d8bf87c42e8929d881a3c75236b946d73ecaf0
bob <bob@bob-ThinkPad-X200.(none)>
2013-11-28 17:29:41 -0500

    simplified the type of data

--- mailcar_0/src/control_car_keyboard.cpp
+++ mailcar_0/src/control_car_keyboard.cpp
@@ -1,7 +1,8 @@
 //reference to : https://github.com/ros/ros_tutorials/blob/hydro-devel/turtlesim/tutorials/teleop_turtle_key.cpp
 
 #include <ros/ros.h>
-#include <geometry_msgs/Twist.h>
+//#include <geometry_msgs/Twist.h>
+#include <std_msgs/Int8.h>
 #include <signal.h>
 #include <termios.h>
 #include <stdio.h>
@@ -19,24 +20,16 @@ public:
   void keyLoop();
 
 private:
-
-  
   ros::NodeHandle nh_;
-  double linear_, angular_, l_scale_, a_scale_;
+  std_msgs::Int8 velocity;
   ros::Publisher twist_pub_;
   
 };
 
-TeleopCar::TeleopCar():
-  linear_(0),
-  angular_(0),
-  l_scale_(2.0),
-  a_scale_(2.0)
+TeleopCar::TeleopCar()
 {
-  nh_.param("scale_angular", a_scale_, a_scale_);
-  nh_.param("scale_linear", l_scale_, l_scale_);
-
-  twist_pub_ = nh_.advertise<geometry_msgs::Twist>("car/cmd_vel", 1);
+  velocity.data = 0;
+  twist_pub_ = nh_.advertise<std_msgs::Int8>("car/cmd_vel", 1);
 }
 
 int kfd = 0;
@@ -92,40 +85,37 @@ void TeleopCar::keyLoop()
       exit(-1);
     }
 
-    linear_=angular_=0;
+    velocity.data=0;
     ROS_DEBUG("value: 0x%02X\n", c);
   
     switch(c)
     {
       case KEYCODE_L:
         ROS_DEBUG("LEFT");
-        angular_ = 1.0;
+        velocity.data = 1;
         dirty = true;
         break;
       case KEYCODE_R:
         ROS_DEBUG("RIGHT");
-        angular_ = -1.0;
+        velocity.data = 2;
         dirty = true;
         break;
       case KEYCODE_U:
         ROS_DEBUG("UP");
-        linear_ = 1.0;
+        velocity.data = 3;
         dirty = true;
         break;
       case KEYCODE_D:
         ROS_DEBUG("DOWN");
-        linear_ = -1.0;
+        velocity.data = 4;
         dirty = true;
         break;
     }
    
-
-    geometry_msgs::Twist twist;
-    twist.angular.z = a_scale_*angular_;
-    twist.linear.x = l_scale_*linear_;
+    
     if(dirty ==true)
     {
-      twist_pub_.publish(twist);    
+      twist_pub_.publish(velocity);    
       dirty=false;
     }
   }
